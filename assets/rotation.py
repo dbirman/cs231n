08d4337aa5cb0dbe3b00f2e49b23c811b15e687e
dylancable <dylan@rescomp-15-289736.stanford.edu>
2016-01-27 00:46:13 -0800

    Rotational Motion
    Added rotational motion

---
+++ assets/rotation.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rd
+import math
+import matplotlib.animation as anim
+
+#############
+# Rotate #
+#############
+#usage
+#r = Rotational(100,100,5,200,30*1.0/180*np.pi,1,20)
+#r.gen()
+#r.plot()
+
+# Rotational motion in 360 degs
+class Rotational():
+
+	# Initialize
+	def __init__(self,imageWidth,imageHeight,t,n_dots,rotational_speed,dot_radius,frames_per_second):
+		# Data size
+		self.time = t
+		self.imageWidth = imageWidth #in pixels
+		self.imageHeight = imageHeight
+		self.maxX = imageWidth/2
+		self.maxY = imageHeight/2
+		self.num_dots = n_dots
+		self.speed = rotational_speed #radians per second
+		self.frames_per_second = frames_per_second
+		self.dot_radius = dot_radius
+
+	# Generate a new motion stimulus
+	def gen(self):
+		time = self.time 
+		imageWidth = self.imageWidth
+		imageHeight = self.imageHeight
+		maxX = self.maxX 
+		maxY = self.maxY 
+		num_dots = self.num_dots 
+		speed = self.speed 
+		frames_per_second = self.frames_per_second 
+		dot_radius = self.dot_radius 
+		length = math.sqrt(math.pow(imageWidth,2)+math.pow(imageHeight,2))
+		position = [[rd.random()*length - length/2,rd.random()*length - length/2] for i in range(num_dots)]
+		#convert to polar coordinates
+		for i in range(num_dots):
+			r = np.sqrt(math.pow(position[i][0],2)+math.pow(position[i][1],2))
+			theta = np.arctan2(position[i][1],position[i][0])
+			position[i][:] = [r,theta]
+
+		self.video = [[[0 for i in range(imageWidth)] for j in range(imageHeight)] for k in range(time*frames_per_second)]
+		for t in range(time*frames_per_second):
+			#convert to cartesian
+			position_cartesian = [[position[i][0]*np.cos(position[i][1]),position[i][0]*np.sin(position[i][1])] for i in range(len(position))]
+			for k in range(len(position_cartesian)):
+				for x in range(int(round(position_cartesian[k][0]))-1-dot_radius,int(round(position_cartesian[k][0]))+dot_radius+1):
+					for y in range(int(round(position_cartesian[k][1]))-1-dot_radius,int(round(position_cartesian[k][1]))+dot_radius+1):
+						i = maxY - y
+						j = x + maxX
+						i = min(max(i,0),imageHeight-1) #constrain within range
+						j = min(max(j,0),imageWidth-1) #constrain within range
+						y_corr = maxY - i
+						x_corr = j - maxX
+						dist = math.sqrt(math.pow(x_corr-position_cartesian[k][0],2) + math.pow(y_corr-position_cartesian[k][1],2))
+						self.video[t][i][j] = min(1,self.video[t][i][j]+math.exp(-math.pow(dist/dot_radius,2)));
+			#rotate position
+			for i in range(num_dots):
+				position[i][1] = position[i][1] + speed/frames_per_second;
+
+	# File saving (for later loading into convnets)
+	def fout(self):
+		print "todo: implement save to file"
+
+	# Display figure showing this motion
+	def plot(self):
+		fig = plt.figure()
+		ax = plt.gca()
+		im = plt.imshow(self.video[0][:][:], cmap='Greys_r', vmin=0, vmax=1)	
+		def up(t):
+			im.set_array(self.video[t][:][:])
+			return im,
+		ani = anim.FuncAnimation(fig, up, np.arange(self.time*self.frames_per_second), interval=1000.0/self.frames_per_second, blit=True, repeat_delay=1000)
+		plt.show()
+		return ani
