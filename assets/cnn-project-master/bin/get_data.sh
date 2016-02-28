d691f75781390e7472a5e6439e4dc26f0f987f00
dan <danbirman@gmail.com>
2016-02-28 16:12:44 -0500

    Folder Cleanup: Removed all dated files.

--- assets/cnn-project-master/bin/get_data.sh
+++
@@ -1,6 +0,0 @@
-cd data
-wget http://crcv.ucf.edu/data/UCF101/UCF101.rar
-unrar x UCF101.rar
-wget http://crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
-unzip UCF101TrainTestSplits-RecognitionTask.zip
-cd ..
